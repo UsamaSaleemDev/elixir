531def7bb04505e81dc8afb4d94af2a0cc8beb66
Usama Saleem <usama.saleem@contrivesolution.com>
2026-08-18 22:45:36 +0500

    Pipe op and tests.

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,21 +1,11 @@
 defmodule Cards do
-  # @moduledoc """
-  # Documentation for `Cards`.
-  # """
+  @moduledoc """
+    Provides methods for creating and handling a deck of `Cards`.
+  """
 
-  # @doc """
-  # Hello world.
-
-  # ## Examples
-
-  #     iex> Cards.hello()
-  #     :world
-
-  # """
-  # def hello do
-  #   :world
-  # end
-
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
@@ -29,10 +19,30 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+  
+  ## Examples
+  
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+  
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, _deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Hearts"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
