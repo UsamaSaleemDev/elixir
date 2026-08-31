496321b2760056b1a06cc24602dea98978103922
Usama Saleem <usama.saleem@contrivesolution.com>
2026-08-31 19:04:02 +0500

    Controllers and migrations.

--- shop/lib/shop_web/controllers/product_html.ex
+++ shop/lib/shop_web/controllers/product_html.ex
@@ -2,4 +2,12 @@ defmodule ShopWeb.ProductHTML do
   use ShopWeb, :html
 
   embed_templates "product_html/*"
+
+  attr :name, :string, required: true
+
+  def product(assigns) do
+    ~H"""
+    <p>Game: {assigns.name}</p>
+    """
+  end
 end
--- shop/lib/shop_web/controllers/random_html.ex
+++ shop/lib/shop_web/controllers/random_html.ex
@@ -1,7 +1,9 @@
 defmodule ShopWeb.RandomHTML do
   use ShopWeb, :html
 
-  def random(_assigns) do
-
+  def random(assigns) do
+    ~H"""
+    <h1>This is the random page!</h1>
+    """
   end
 end
